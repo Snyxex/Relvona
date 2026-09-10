ff82b8c2c61ee4c07c57b4c74f195f3b01d0163f
Jason <153672410+Snyxex@users.noreply.github.com>
2026-09-10 10:52:21 +0200

    chore(frontend): remove unused dashboard tab barrel

--- frontend/src/components/dashboard-tabs/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as OverviewTab } from "./overview-tab";
-export { default as AnalyticsTab } from "./analytics-tab";
-export { default as AgentsTab } from "./agents-tab";
-export { default as CustomersTab } from "./customers-tab";
